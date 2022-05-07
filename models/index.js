163a5f138e3aa587940a827ab9f222cfc50eb0ad
Jonathan Bridge <jvbridge@gmail.com>
2022-05-07 08:53:36 -0700

    added event relationship

--- models/index.js
+++ models/index.js
@@ -1,4 +1,13 @@
 const Users = require("./users");
-const Events = requre("./events");
+const Events = require("./events");
+
+Users.hasMany(Events, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+Events.belongsTo(Users, {
+  foreignKey: "user_id",
+});
 
 module.exports = { Users, Events };
