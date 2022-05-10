c0d4b71d6582f40482207697688e35e438616d9a
Jonathan Bridge <jvbridge@gmail.com>
2022-05-10 14:33:30 -0700

    log in api

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,4 +3,7 @@ const router = require("express").Router();
 const events = require("./events");
 router.use("/events", events);
 
+const users = require("./users");
+router.use("/users", users);
+
 module.exports = router;
