ed71411cc6cc61526cdec04f5dfcc3b571a59e5e
KuyaDevin <devinnguyen@ucsb.edu>
2022-05-10 11:59:48 -0700

    Added route to get upcoming events page, still needs help that will determine upcoming events

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -44,6 +44,28 @@ router.get('event/:id', async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+//Route to upcoming events(still need to add helper that will determine upcoming events)
+router.get('/upcoming', async (req, res) => {
+    try {
+        const eventData = await Events.findAll ({
+            include :[
+                {
+                    model: Users,
+                    attributes: ['name'],
+                }
+            ]
+        });
+        const events = eventData.map((event) => event.get({ plain: true }));
+
+        res.render('upcoming', {
+            events,
+            logged_in: req.session
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 // Route to login
 router.get('/login', (req, res) => {
     if (req.session.logged_in) {
