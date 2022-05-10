5a345ef2ee85c14de570850a7c9401b1375bfc3d
KuyaDevin <devinnguyen@ucsb.edu>
2022-05-10 11:35:17 -0700

    Added route to login to homeRoutes.js

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -1,3 +1,13 @@
 const router = require("express").Router();
+const { Events, Checklists, Users } = require('../models');
+
+// Route to login
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+        res,redirect('/')
+    }
+
+    res.render('login');
+});
 
 module.exports = router;
