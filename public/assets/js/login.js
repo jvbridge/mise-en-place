b64c8d576dd0a0851920b7a069d1e233e5b29f30
KuyaDevin <devinnguyen@ucsb.edu>
2022-05-10 19:51:58 -0700

    Added a form handler and event listener for the signup modal to login.js in public

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -20,6 +20,28 @@ async function loginFormHandler(event) {
             alert(result.message)
         }
     }
-}
+};
 
-document.querySelector('.modal-body').addEventListener('button')
+async function signupEventHandler(event) {
+    event.preventDefault();
+
+    const name = document.querySelector('#inputUsername').value.trim();
+    const email = document.querySelector('#inputEmail').value.trim();
+    const password = document.querySelector('#inputPassword').value.trim();
+
+    if (name && email && password) {
+        const response = await fetch('/api/user', {
+            method: 'POST',
+            body: JSON.stringify({ name, email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/calendar');
+        } else {
+            alert(result.message);
+        }
+    }
+};
+
+document.querySelector('.modal-body').addEventListener('button');
