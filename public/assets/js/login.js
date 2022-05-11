b3d1b2278333bf9e07b48318f2a8c3d9db9e7cfb
KuyaDevin <devinnguyen@ucsb.edu>
2022-05-10 19:12:28 -0700

    Made login.js to have a form handler

---
+++ public/assets/js/login.js
@@ -0,0 +1,17 @@
+async function loginFormHandler(event) {
+    event.preventDefault();
+
+    const email= document.querySelector('#inputUsername').value.trim();
+    const password = document.querySelector('#inputPassword').value.trim();
+
+    if (email && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'post',
+            body: JSON.stringify({
+                email,
+                password
+            }),
+            headers: {'Content-Type': 'application/json'}
+        });
+    }
+}
