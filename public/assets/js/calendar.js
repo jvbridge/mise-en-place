fc0289c5966b801acbe99da44c8e71e7fc5955b5
KuyaDevin <devinnguyen@ucsb.edu>
2022-05-11 11:47:46 -0700

    Added headerToolbar to calendar.js in public folder

--- public/assets/js/calendar.js
+++ public/assets/js/calendar.js
@@ -1,8 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendar');
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-        initalView: 'dayGridMonth',
-        selectable: 'true'
-    });
-    calendar.render();
+document.addEventListener("DOMContentLoaded", function () {
+  var calendarEl = document.getElementById("calendar");
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    initalView: "dayGridMonth",
+    selectable: "true",
+    headerToolbar: {
+      left: "prev,next today",
+      center: "title",
+      right: "dayGridMonth,timeGrid",
+    },
+  });
+  calendar.render();
 });
