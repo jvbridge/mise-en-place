7f1ba706587d283072c4e0588eb77d1d57884993
Jonathan Bridge <jvbridge@gmail.com>
2022-05-12 10:59:42 -0700

    create new item

--- controllers/api/checklist.js
+++ controllers/api/checklist.js
@@ -122,4 +122,35 @@ router.put("/:id", authDeny, async (req, res) => {
   }
 });
 
+// add an item to the checklist
+router.post("/:id/item", authDeny, async (req, res) => {
+  try {
+    const checklistData = await Checklists.findOne({
+      where: { id: req.params.id },
+      include: [{ model: ChecklistItems, required: false }],
+    });
+
+    if (!checklistData) {
+      res.status(404).json(`Found no checklists with id: ${req.params.id}`);
+      return;
+    }
+
+    if (checklistData.user_id !== req.session.userId) {
+      res.status(403).json("User does not own that checklist");
+      return;
+    }
+
+    const item = {
+      checklist_id: checklistData.id,
+      description: req.body.description,
+    };
+
+    checklistItemData = await ChecklistItems.create(item);
+
+    res.sendStatus(200);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
