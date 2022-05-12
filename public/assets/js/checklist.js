63bfe0f7de6c235c6a45a2df7ef85d62e6423c81
miacarmen <maitreyacarmen@gmail.com>
2022-05-12 11:24:05 -0700

    changes to checklist handlebars, started checklist public js

--- public/assets/js/checklist.js
+++ public/assets/js/checklist.js
@@ -1,7 +1,23 @@
 // when icon clicked
-// then display input text box 
+const addBtn = document
+  .getElementById("addcheck");
+//   .addEventListener("click", newCheckForm);
+
 // when add item is clicked
-// then input is added to the list with empty checkbox
+const newCheckForm = (event) => {
+    event.preventDefault();
+
+  addBtn.setAttribute("aria-hidden", true);
+
+  const addCheckForm = document.getElementById("addCheck-form");
+  // then display input text box
+  if (addCheckForm.style.display === "none") {
+    addCheckForm.style.display = "inline";
+  } else {
+    addCheckForm.style.display = "none";
+  }
+  // then input is added to the list with empty checkbox
+};
 
 // add edit button ?
 
