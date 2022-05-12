1e3c5767175bc7643bb6fead6e06c5df7ca96908
Jonathan Bridge <jvbridge@gmail.com>
2022-05-12 11:15:47 -0700

    toggle item

--- controllers/api/checklist.js
+++ controllers/api/checklist.js
@@ -196,4 +196,48 @@ router.put("/:id/item/:index", authDeny, async (req, res) => {
   }
 });
 
+// post route to toggle the item from checked to unchecked
+router.post("/:id/item/:index/toggle", authDeny, async (req, res) => {
+  try {
+    const checklistData = await Checklists.findOne({
+      where: { id: req.params.id },
+      include: [{ model: ChecklistItems, required: false }],
+    });
+
+    if (!checklistData) {
+      res.status(404).json(`Found no checklists with id: ${req.params.id}`);
+      return;
+    }
+
+    if (checklistData.user_id !== req.session.userId) {
+      res.status(403).json("User does not own that checklist");
+      return;
+    }
+
+    const itemToMod = checklistData.checklist_items[req.params.index];
+
+    if (!itemToMod) {
+      res.status(404).json("index is out of bounds");
+      return;
+    }
+
+    // get a sequelize object of the item
+    const itemData = await ChecklistItems.findByPk(itemToMod.id);
+
+    // same everything except the completion
+    const item = {
+      checklist_id: checklistData.id,
+      description: itemData.description,
+      is_complete: !itemData.is_complete,
+    };
+
+    itemData.set(item);
+    await itemData.save();
+
+    res.sendStatus(200);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
